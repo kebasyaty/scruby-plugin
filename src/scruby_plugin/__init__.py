231559331eedb81260aa4ac711b531aef83432c4
kebasyaty <kebasyaty@gmail.com>
2026-01-12 17:04:10 +0200

    add a ScrubyPlugin class

--- src/scruby_plugin/__init__.py
+++ src/scruby_plugin/__init__.py
@@ -1,4 +1,17 @@
-# Scruby-Plugin - Library for creating Scruby plugins.
 # Copyright (c) 2026 Gennady Kostyunin
 # SPDX-License-Identifier: MIT
-"""Description modul."""
+"""Scruby-Plugin - Library for creating Scruby plugins."""
+
+from __future__ import annotations
+
+__all__ = ("ScrubyPlugin",)
+
+import weakref
+from typing import Any
+
+
+class ScrubyPlugin:  # noqa: RUF067
+    """Base class for creating Scruby plugins."""
+
+    def __init__(self, scruby: Any) -> None:  # noqa: D107
+        self.scruby = weakref.ref(scruby)
