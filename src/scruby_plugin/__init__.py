a1bce0ca938d8225126ed7dc28fa8f46caf40383
kebasyaty <kebasyaty@gmail.com>
2026-01-19 16:43:04 +0200

    fix RUF067

--- src/scruby_plugin/__init__.py
+++ src/scruby_plugin/__init__.py
@@ -6,12 +6,4 @@ from __future__ import annotations
 
 __all__ = ("ScrubyPlugin",)
 
-import weakref
-from typing import Any
-
-
-class ScrubyPlugin:  # noqa: RUF067
-    """Base class for creating Scruby plugins."""
-
-    def __init__(self, scruby: Any) -> None:  # noqa: D107
-        self.scruby = weakref.ref(scruby)
+from scruby_plugin.plugin import ScrubyPlugin
---
+++ src/scruby_plugin/plugin.py
@@ -0,0 +1,18 @@
+# Scruby-Plugin - Library for creating Scruby plugins.
+# Copyright (c) 2026 Gennady Kostyunin
+# SPDX-License-Identifier: MIT
+"""Contains the base class for creating Scruby plugins."""
+
+from __future__ import annotations
+
+__all__ = ("ScrubyPlugin",)
+
+import weakref
+from typing import Any
+
+
+class ScrubyPlugin:
+    """Base class for creating Scruby plugins."""
+
+    def __init__(self, scruby: Any) -> None:  # noqa: D107
+        self.scruby = weakref.ref(scruby)
